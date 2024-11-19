d5e4bfefcf74973c39ac7b03180f0e5e368229f9
fwcd <fwcd@fwcd.dev>
2024-11-20 00:48:04 +0100

    Emit a pure 440 Hz sine tone

--- Fourier Synth.swiftpm/Sources/Audio/Synthesizer.swift
+++ Fourier Synth.swiftpm/Sources/Audio/Synthesizer.swift
@@ -12,31 +12,39 @@ import Foundation
 
 class Synthesizer: ObservableObject {
     private let engine: AVAudioEngine
+    private var phase: Float = 0
     
     init() throws {
         engine = AVAudioEngine()
-        
-        let srcNode = AVAudioSourceNode { _, _, frameCount, buffers in
-            let buffers = UnsafeMutableAudioBufferListPointer(buffers)
-            for i in 0..<Int(frameCount) {
-                for buffer in buffers {
-                    let buffer = UnsafeMutableBufferPointer<Float>(buffer)
-                    buffer[i] = sin(Float(i) * 0.005)
-                }
-            }
-            return noErr
-        }
         
         let mainMixer = engine.mainMixerNode
         let outputNode = engine.outputNode
         
         let outputFormat = outputNode.inputFormat(forBus: 0)
+        let sampleRate = Float(outputFormat.sampleRate)
+        
         let inputFormat = AVAudioFormat(
             commonFormat: outputFormat.commonFormat,
             sampleRate: outputFormat.sampleRate,
             channels: 1,
             interleaved: outputFormat.isInterleaved
         )
+        
+        let srcNode = AVAudioSourceNode { [self] _, _, frameCount, buffers in
+            let buffers = UnsafeMutableAudioBufferListPointer(buffers)
+            let frequency: Float = 440
+            let phaseIncrement = (2 * .pi / sampleRate) * frequency
+            for frame in 0..<Int(frameCount) {
+                for buffer in buffers {
+                    let buffer = UnsafeMutableBufferPointer<Float>(buffer)
+                    buffer[frame] = sin(Float(phase))
+                }
+                phase += phaseIncrement
+            }
+            return noErr
+        }
+        
+        
         
         engine.attach(srcNode)
         engine.connect(srcNode, to: mainMixer, format: inputFormat)
