34b418ba2f425091b6601176926804b30faca923
fwcd <fwcd@fwcd.dev>
2024-11-24 23:36:01 +0100

    Add overlay

--- Sineweaver.swiftpm/Sources/View/SineweaverView.swift
+++ Sineweaver.swiftpm/Sources/View/SineweaverView.swift
@@ -6,5 +6,17 @@ import Combine
 struct SineweaverView: View {
     var body: some View {
         SpriteView(scene: SineweaverScene())
+            .overlay(alignment: .bottomTrailing) {
+                HStack {
+                    Button("Test") {
+                        print("Test")
+                    }
+                }
+                .buttonStyle(.borderedProminent)
+                .padding()
+                .background(Color(ViewDefaults.quaternary))
+                .clipShape(RoundedRectangle(cornerRadius: ViewDefaults.cornerRadius))
+                .padding()
+            }
     }
 }
