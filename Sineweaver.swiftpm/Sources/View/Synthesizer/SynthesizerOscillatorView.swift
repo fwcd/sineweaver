e0d86fc1b79a7e9aeb731836deb912427ad83b11
fwcd <fwcd@fwcd.dev>
2024-12-25 02:40:05 +0100

    Animate the oscillator

--- Sineweaver.swiftpm/Sources/View/Synthesizer/SynthesizerOscillatorView.swift
+++ Sineweaver.swiftpm/Sources/View/Synthesizer/SynthesizerOscillatorView.swift
@@ -10,14 +10,18 @@ import SwiftUI
 struct SynthesizerOscillatorView<Node>: View where Node: SynthesizerNodeProtocol {
     let node: Node
     
+    @State private var referenceDate = Date()
+    
     var body: some View {
         let displaySampleRate: Double = 2000
         let displayInterval: TimeInterval = 0.2
-        ChartView(sampleCount: Int(displaySampleRate * displayInterval)) { output in
-            node.render(inputs: [], output: &output, context: .init(
-                frame: 0,
-                sampleRate: Double(displaySampleRate)
-            ))
+        TimelineView(.animation) { context in
+            ChartView(sampleCount: Int(displaySampleRate * displayInterval)) { output in
+                node.render(inputs: [], output: &output, context: .init(
+                    frame: Int(context.date.timeIntervalSince(referenceDate) * displaySampleRate),
+                    sampleRate: Double(displaySampleRate)
+                ))
+            }
         }
     }
 }
