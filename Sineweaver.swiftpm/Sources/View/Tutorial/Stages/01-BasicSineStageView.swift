e09f5c92cabecccb767ca620ead727d6485a3ef1
fwcd <fwcd@fwcd.dev>
2024-12-25 02:36:42 +0100

    Make oscillator slider logarithmic

--- Sineweaver.swiftpm/Sources/View/Tutorial/Stages/01-BasicSineStageView.swift
+++ Sineweaver.swiftpm/Sources/View/Tutorial/Stages/01-BasicSineStageView.swift
@@ -15,7 +15,7 @@ struct BasicSineStageView: View {
     var body: some View {
         VStack {
             SynthesizerOscillatorView(node: SineNode(frequency: frequency))
-            Slider(value: $frequency.logarithmic, in: 20...20000)
+            Slider(value: $frequency.logarithmic, in: log(20)...log(20000))
         }
     }
 }
