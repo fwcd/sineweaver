d83b3cb8c0ef2e75ec467cde8b64caae17a4788c
fwcd <fwcd@fwcd.dev>
2025-01-04 21:41:39 +0100

    Display currently playing note in oscillator

--- Sineweaver.swiftpm/Sources/View/Synthesizer/SynthesizerOscillatorView.swift
+++ Sineweaver.swiftpm/Sources/View/Synthesizer/SynthesizerOscillatorView.swift
@@ -9,7 +9,12 @@ import SwiftUI
 
 struct SynthesizerOscillatorView: View {
     @Binding var node: OscillatorNode
+    var tuning: any Tuning = EqualTemperament()
     
+    private var note: Note {
+        Note(semitone: Int(tuning.semitone(for: node.frequency).rounded()))
+    }
+
     private var playingNode: OscillatorNode {
         var node = node
         node.isPlaying = true
@@ -22,13 +27,21 @@ struct SynthesizerOscillatorView: View {
                 SynthesizerChartView(node: playingNode)
                     .opacity(node.isPlaying ? 1 : 0.5)
                     .overlay(alignment: .bottom) {
-                        Text(String(format: "%d Hz (%.2f dB)", Int(node.frequency), 10 * log10(node.volume)))
-                            .font(.system(size: 14).monospacedDigit())
-                            .padding(10)
-                            .background(.ultraThinMaterial)
-                            .clipShape(RoundedRectangle(cornerRadius: 10))
-                            .opacity(node.isPlaying ? 1 : 0)
-                            .animation(.default, value: node.isPlaying)
+                        HStack(spacing: 20) {
+                            HStack {
+                                Text("\(Int(node.frequency)) Hz")
+                                if node.prefersPianoView {
+                                    Text("(aka. \(note))")
+                                }
+                            }
+                            Text(String(format: "%.2f dB", 10 * log10(node.volume)))
+                        }
+                        .font(.system(size: 14).monospacedDigit())
+                        .padding(10)
+                        .background(.ultraThinMaterial)
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                        .opacity(node.isPlaying ? 1 : 0)
+                        .animation(.default, value: node.isPlaying)
                     }
                 Slider2D(
                     size: node.prefersPianoView ? 100 : 200,
@@ -46,7 +59,7 @@ struct SynthesizerOscillatorView: View {
             if node.prefersPianoView {
                 PianoView(notes: Note(.c, 3)..<Note(.c, 6)) { notes in
                     if let note = notes.first {
-                        node.frequency = EqualTemperament().pitchHz(for: note)
+                        node.frequency = tuning.pitchHz(for: note)
                     }
                     node.isPlaying = !notes.isEmpty
                 }
