e5bd8a66666990da1fe04f246542584d148ace9c
fwcd <fwcd@fwcd.dev>
2025-01-04 02:38:38 +0100

    Update description

--- Sineweaver.swiftpm/Sources/ViewModel/Tutorial/SynthesizerStage.swift
+++ Sineweaver.swiftpm/Sources/ViewModel/Tutorial/SynthesizerStage.swift
@@ -23,7 +23,7 @@ enum SynthesizerStage: Hashable, CaseIterable {
         case .basicOscillator:
             [
                 #"At the most fundamental level, a synthesizer produces sounds by sampling a periodic function, commonly a sine wave. The synthesizer presented here is called an "oscillator" and it forms the fundamental building block of almost every form of audio synthesis."#,
-                "This oscillator has two parameters: Frequency (or pitch) and volume. Try moving the slider on the right-hand side to play the synth and control the parameters.",
+                "This oscillator has two parameters: Frequency (or pitch) and volume. Press and drag the slider on the right-hand side to play the synth and control the parameters.",
             ]
         }
     }
