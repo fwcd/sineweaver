76446b3274beff1ae4c16d84791ff6056db98ead
fwcd <fwcd@fwcd.dev>
2025-01-05 02:12:07 +0100

    Remove fixed todo

--- Sineweaver.swiftpm/Sources/View/Synthesizer/SynthesizerView.swift
+++ Sineweaver.swiftpm/Sources/View/Synthesizer/SynthesizerView.swift
@@ -14,7 +14,6 @@ struct SynthesizerView: View {
         HStack {
             ForEach(model.nodes.sorted { $0.key < $1.key }, id: \.key) { node in
                 ComponentBox(node.value.type.name) {
-                    // TODO: Binding
                     let key: UUID = node.key
                     SynthesizerNodeView(node: $model.nodes[key].unwrapped)
                 }
