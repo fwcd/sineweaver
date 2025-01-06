39fae4b88bd144c6935caddb6ffef957b035aba7
fwcd <fwcd@fwcd.dev>
2025-01-06 01:17:19 +0100

    Add support for amplitude modulation to MixerNode

--- Sineweaver.swiftpm/Sources/Model/Synthesizer/Node/MixerNode.swift
+++ Sineweaver.swiftpm/Sources/Model/Synthesizer/Node/MixerNode.swift
@@ -5,13 +5,36 @@
 //  Created on 22.11.24
 //
 
-/// A simple sum of its inputs.
+/// A node that aggregates its inputs in some form.
 struct MixerNode: SynthesizerNodeProtocol {
+    enum Operation: Hashable, Codable {
+        /// Adds the signals together. Corresponds to basic mixing.
+        case sum
+        /// Multiplies the signals together. Corresponds to amplitude modulation.
+        case product
+        
+        var neutral: Double {
+            switch self {
+            case .sum: 0
+            case .product: 1
+            }
+        }
+        
+        func apply(_ lhs: Double, _ rhs: Double) -> Double {
+            switch self {
+            case .sum: lhs + rhs
+            case .product: lhs * rhs
+            }
+        }
+    }
+    
+    var operation: Operation = .sum
+    
     func render(inputs: [SynthesizerNodeInput], output: inout [Double], state: inout Void, context: SynthesizerContext) -> Bool {
         for i in 0..<output.count {
-            output[i] = 0
+            output[i] = operation.neutral
             for input in inputs {
-                output[i] += input.buffer[i]
+                output[i] = operation.apply(output[i], input.buffer[i])
             }
         }
         return true
