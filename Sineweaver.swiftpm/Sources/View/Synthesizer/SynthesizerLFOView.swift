076399acdf7fbe4403bba0fc5fd77affc50c5d4c
fwcd <fwcd@fwcd.dev>
2025-01-06 01:11:02 +0100

    Add frequency dial to LFO

--- Sineweaver.swiftpm/Sources/View/Synthesizer/SynthesizerLFOView.swift
+++ Sineweaver.swiftpm/Sources/View/Synthesizer/SynthesizerLFOView.swift
@@ -14,14 +14,19 @@ struct SynthesizerLFOView: View {
     
     var body: some View {
         let size = ComponentDefaults.padSize / 2
-        TimelineView(.animation) { context in
-            SynthesizerChartView(
-                node: node,
-                timeInterval: context.date.timeIntervalSince(referenceDate),
-                displaySampleRate: 30,
-                displayInterval: 1
-            )
-            .frame(width: size, height: size)
+        VStack(spacing: SynthesizerViewDefaults.vSpacing) {
+            TimelineView(.animation) { context in
+                SynthesizerChartView(
+                    node: node,
+                    timeInterval: context.date.timeIntervalSince(referenceDate),
+                    displaySampleRate: 30,
+                    displayInterval: 1
+                )
+                .frame(width: size, height: size)
+            }
+            LabelledKnob(value: $node.frequency, range: 0.01...1, text: "Frequency") {
+                String(format: "%.2f Hz", $0)
+            }
         }
     }
 }
