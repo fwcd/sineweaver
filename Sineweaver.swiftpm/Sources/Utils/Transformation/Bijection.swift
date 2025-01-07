0e0eec5cbc7dc0ca457644ee41e1ff9533cdb572
fwcd <fwcd@fwcd.dev>
2025-01-07 01:37:24 +0100

    Remove unused transformation utils

--- Sineweaver.swiftpm/Sources/Utils/Transformation/Bijection.swift
+++
@@ -1,9 +0,0 @@
-/// An invertible function.
-protocol Bijection {
-    associatedtype Input
-    associatedtype Output
-    
-    func apply(_ value: Input) -> Output
-    
-    func inverseApply(_ value: Output) -> Input
-}
