d9f3dfb1532e5588e203e01429ebcccdbe68024d
fwcd <fwcd@fwcd.dev>
2025-01-12 22:08:34 +0100

    Disable Catalyst title bar

--- Sineweaver.swiftpm/Sources/SineweaverApp.swift
+++ Sineweaver.swiftpm/Sources/SineweaverApp.swift
@@ -3,8 +3,30 @@ import SwiftUI
 @MainActor private let synthesizer = SynthesizerViewModel()
 @MainActor private let tutorial = TutorialViewModel(synthesizer: synthesizer)
 
+private class SceneDelegate: NSObject, UIWindowSceneDelegate {
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        #if targetEnvironment(macCatalyst)
+        if let titlebar = windowScene.titlebar {
+            titlebar.titleVisibility = .hidden
+        }
+        #endif
+    }
+}
+
+private class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        let sceneConfig = UISceneConfiguration(name: nil, sessionRole: connectingSceneSession.role)
+        sceneConfig.delegateClass = SceneDelegate.self
+        return sceneConfig
+    }
+}
+
 @main
 struct SineweaverApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             SineweaverView()
