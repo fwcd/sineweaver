6f0bb8871c3afef22d124e7bff821142120020bf
fwcd <fwcd@fwcd.dev>
2025-01-15 01:46:29 +0100

    Clean up style issues in Complex

--- Sineweaver.swiftpm/Sources/View/Utils/Complex.swift
+++ Sineweaver.swiftpm/Sources/View/Utils/Complex.swift
@@ -9,26 +9,26 @@ import Foundation
 
 /// A complex number, i.e. an element of the algebraic closure of the real numbers.
 public struct Complex: SignedNumeric, Hashable, ExpressibleByIntegerLiteral, ExpressibleByFloatLiteral, CustomStringConvertible, Sendable {
-    public static let i = Complex(0, i: 1)
+    public static let i = Self(0, i: 1)
     public var real: Double
     public var imag: Double
-    public var description: String { return "\(real) + \(imag)i" }
-    public var magnitudeSquared: Double { return (real * real) + (imag * imag) }
-    public var magnitude: Double { return magnitudeSquared.squareRoot() }
-    public var absolute: Double { return magnitude }
-    public var squared: Complex { return self * self }
-    public var conjugate: Complex { return Complex(real, i: -imag) }
+    public var description: String { "\(real) + \(imag)i" }
+    public var magnitudeSquared: Double { (real * real) + (imag * imag) }
+    public var magnitude: Double { magnitudeSquared.squareRoot() }
+    public var absolute: Double { magnitude }
+    public var squared: Self { self * self }
+    public var conjugate: Self { Self(real, i: -imag) }
 
-    public var exp: Complex {
+    public var exp: Self {
         guard imag != 0 else {
-            return Complex(Foundation.exp(real))
+            return Self(Foundation.exp(real))
         }
         
         guard real != 0 else {
-            return Complex(cos(imag), i: sin(imag))
+            return Self(cos(imag), i: sin(imag))
         }
         
-        return Complex(real).exp * Complex(i: imag).exp
+        return Self(real).exp * Self(i: imag).exp
     }
 
     public init(_ real: Double = 0, i imag: Double = 0) {
@@ -48,32 +48,32 @@ public struct Complex: SignedNumeric, Hashable, ExpressibleByIntegerLiteral, Exp
         self.init(value)
     }
 
-    public static func +(lhs: Complex, rhs: Complex) -> Complex {
-        return Complex(lhs.real + rhs.real, i: lhs.imag + rhs.imag)
+    public static func +(lhs: Self, rhs: Self) -> Self {
+        Self(lhs.real + rhs.real, i: lhs.imag + rhs.imag)
     }
 
-    public static func -(lhs: Complex, rhs: Complex) -> Complex {
-        return Complex(lhs.real - rhs.real, i: lhs.imag - rhs.imag)
+    public static func -(lhs: Self, rhs: Self) -> Self {
+        Self(lhs.real - rhs.real, i: lhs.imag - rhs.imag)
     }
 
-    public static func +=(lhs: inout Complex, rhs: Complex) {
+    public static func +=(lhs: inout Self, rhs: Self) {
         lhs.real += rhs.real
         lhs.imag += rhs.imag
     }
 
-    public static func -=(lhs: inout Complex, rhs: Complex) {
+    public static func -=(lhs: inout Self, rhs: Self) {
         lhs.real -= rhs.real
         lhs.imag -= rhs.imag
     }
 
-    public static func *=(lhs: inout Complex, rhs: Complex) {
+    public static func *=(lhs: inout Self, rhs: Self) {
         let newReal = (lhs.real * rhs.real) - (lhs.imag * rhs.imag)
         let newImag = (lhs.real * rhs.imag) + (lhs.imag * rhs.real)
         lhs.real = newReal
         lhs.imag = newImag
     }
 
-    public static func /=(lhs: inout Complex, rhs: Complex) {
+    public static func /=(lhs: inout Self, rhs: Self) {
         let denominator = (rhs.real * rhs.real) + (rhs.imag * rhs.imag)
         let newReal = ((lhs.real * rhs.real) + (lhs.imag * rhs.imag)) / denominator
         let newImag = ((lhs.imag * rhs.real) - (lhs.real * rhs.imag)) / denominator
@@ -81,29 +81,29 @@ public struct Complex: SignedNumeric, Hashable, ExpressibleByIntegerLiteral, Exp
         lhs.imag = newImag
     }
 
-    public static func *(lhs: Complex, rhs: Double) -> Complex {
-        return Complex(lhs.real * rhs, i: lhs.imag * rhs)
+    public static func *(lhs: Self, rhs: Double) -> Self {
+        Self(lhs.real * rhs, i: lhs.imag * rhs)
     }
 
-    public static func *(lhs: Complex, rhs: Int) -> Complex {
-        return lhs * Double(rhs)
+    public static func *(lhs: Self, rhs: Int) -> Self {
+        lhs * Double(rhs)
     }
 
-    public static func /(lhs: Complex, rhs: Double) -> Complex {
-        return Complex(lhs.real / rhs, i: lhs.imag / rhs)
+    public static func /(lhs: Self, rhs: Double) -> Self {
+        Self(lhs.real / rhs, i: lhs.imag / rhs)
     }
 
-    public static func /(lhs: Complex, rhs: Int) -> Complex {
-        return lhs / Double(rhs)
+    public static func /(lhs: Self, rhs: Int) -> Self {
+        lhs / Double(rhs)
     }
 
-    public static func *(lhs: Complex, rhs: Complex) -> Complex {
+    public static func *(lhs: Self, rhs: Self) -> Self {
         var result = lhs
         result *= rhs
         return result
     }
 
-    public static func /(lhs: Complex, rhs: Complex) -> Complex {
+    public static func /(lhs: Self, rhs: Self) -> Self {
         var result = lhs
         result /= rhs
         return result
@@ -114,12 +114,11 @@ public struct Complex: SignedNumeric, Hashable, ExpressibleByIntegerLiteral, Exp
         imag.negate()
     }
 
-    public prefix static func -(operand: Complex) -> Complex {
-        return Complex(-operand.real, i: -operand.imag)
+    public prefix static func -(operand: Self) -> Self {
+        Self(-operand.real, i: -operand.imag)
     }
 
-    public func equals(_ rhs: Complex, accuracy: Double) -> Bool {
-        return (real - rhs.real).magnitude < accuracy
-            && (imag - rhs.imag).magnitude < accuracy
+    public func equals(_ rhs: Self, accuracy: Double) -> Bool {
+        (real - rhs.real).magnitude < accuracy && (imag - rhs.imag).magnitude < accuracy
     }
 }
